d94d12f971e36749a922bcf50d6bf0a3e9c01867
Tom <tom@lant.uk>
2019-09-19 13:12:21 +0100

    Addressing review comments

--- scripts/versions.py
+++ scripts/versions.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 from __future__ import print_function
 import os
 import sys
@@ -7,13 +6,24 @@ import argparse
 from subprocess import check_output
 from collections import namedtuple
 
-parser = argparse.ArgumentParser(description='Should be run from the root of the git repo. Given a source policy document, will spelunk the git commit history to find the most recent commit for every version number, and generate an instance of the document containing the policy text at that point in time.. The version number will be inserted into the filename (immediately prior to the file extension). Will overwrite any existing files blindly. Will not delete output from previous executions.')
+parser = argparse.ArgumentParser(
+        description=('Should be run from the root of the git repo. Given a source '
+                     'policy document, will spelunk the git commit history to find '
+                     'the most recent commit for every version number, and generate '
+                     'an instance of the document containing the policy text at that'
+                     ' point in time. The version number will be inserted into the '
+                     'filename (immediately prior to the file extension). Will '
+                     'overwrite any existing files blindly. Will not delete output '
+                     'from previous executions.'))
 parser.add_argument('filepath',
-                    help='File path to the source policy file from which to generate the versions.')
+                    help=('File path to the source policy file from which to generate'
+                    ' the versions.'))
 parser.add_argument('--destination', default='versions',
                     help='Destination root to output the versioned files')
 parser.add_argument('--unversioned', default=None,
-                    help='Version number to use for unversioned commits. Defaults to None, which causes no file to be generated for policy commits without version information')
+                    help=('Version number to use for unversioned commits. Defaults to'
+                    'None, which causes no file to be generated for policy commits '
+                    'without version information'))
 args = parser.parse_args()
 
 Version = namedtuple('Version', ['commit', 'meta', 'text'])
@@ -30,16 +40,37 @@ def list_commits(filepath):
 
 
 def get_meta(text):
+    """Metadata, if present, appears at the top of the markdown file, as key/value
+    pairs, sandwiched between two rows of `---`, e.g.:
+    ```
+    ---
+    key1: value1
+    key2: lorem ipsum
+    ---
+    ```
+    """
+
+    # We want to ignore empty lines for the purposes of extracting metadata
     text = [line for line in text
             if line.strip()]
+    for line in text:
+        print(line)
+
     index = [i for i, j in enumerate(text) if j == '---']
-    if len(index) >= 2 and index[0] == 0:
-        # Ladies and gentlemen - we have a file with meta.
-        meta = dict([[x.strip() for x in line.split(':')]
-                     for line
-                     in text[index[0]+1:index[1]]])
-        return meta
-    return {}
+    print(index)
+
+    if len(index) != 2:
+        return {}
+
+    [metastart, metaend] = index
+    if metastart != 0:
+        return {}
+
+    # Ladies and gentlemen - we have a file with meta.
+    meta = dict([[x.strip() for x in line.split(':')]
+                 for line
+                 in text[metastart+1:metaend]])
+    return meta
 
 
 def get_version(filepath, commit):
@@ -48,7 +79,7 @@ def get_version(filepath, commit):
                                    '%s:%s' % (commit, filepath)]).decode('utf-8')
     # Either subprocess or git show is adding an extra newline at the end; let's
     # get rid of it.
-    text = [row for row in command_output.split('\n')][0:-1]
+    text = command_output.split('\n')[0:-1]
     meta = get_meta(text)
     return Version(commit=commit,
                    meta=meta,
@@ -69,21 +100,24 @@ for commit in list_commits(args.filepath):
     if version_number is not None:
         versions[version_number] = version
 
-print('%s:' % args.filepath)
-for version_number, version in versions.items():
-    # Stripping 'docs/' from the start of the path so that the directory structure
-    # of the output dir (defaulting to `versions`) can mirror the source `docs`
-    # directory.
-    path = '%s/%s' % (args.destination, os.path.dirname(args.filepath)[len('docs/'):])
-    if not os.path.exists(path):
-        os.makedirs(path)
-    destination_filepath = '%s/%s' % (
-            path,
-            versioned_filename(args.filepath, version_number)
-            )
-    with open(destination_filepath, 'w') as f:
-        print('%s v%s -> %s' % (
-            version.commit,
-            version_number,
-            destination_filepath), file=sys.stderr)
-        f.writelines('\n'.join(version.text))
+if len(versions) == 0:
+    print('%s: no versions found' % args.filepath, file=sys.stderr)
+else:
+    print('%s:' % args.filepath, file=sys.stderr)
+    for version_number, version in versions.items():
+        # Stripping 'docs/' from the start of the path so that the directory structure
+        # of the output dir (defaulting to `versions`) can mirror the source `docs`
+        # directory.
+        path = '%s/%s' % (args.destination, os.path.dirname(args.filepath)[len('docs/'):])
+        if not os.path.exists(path):
+            os.makedirs(path)
+        destination_filepath = '%s/%s' % (
+                path,
+                versioned_filename(args.filepath, version_number)
+                )
+        with open(destination_filepath, 'w') as f:
+            print('%s v%s -> %s' % (
+                version.commit,
+                version_number,
+                destination_filepath), file=sys.stderr)
+            f.writelines('\n'.join(version.text))
